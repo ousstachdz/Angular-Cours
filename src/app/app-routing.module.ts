01bc5c2179b99b41cfa94a98830a51fe6604bc28
ousstachdz <oussama.tachi@outlook.com>
2022-02-17 02:55:33 +0100

    routing cours

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'student' , component: StudentComponent},
+  {path : 'teacher' , component: TeacherComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutesComponent = [
+  StudentComponent, TeacherComponent,
+  ]
+
